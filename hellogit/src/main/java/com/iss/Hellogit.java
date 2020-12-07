3a9c457e485e110300fbf6bd48c60b0387783f7c
GZY <923256697@qq.com>
2020-12-07 16:31:15 +0800

    返回

--- hellogit/src/main/java/com/iss/Hellogit.java
+++ hellogit/src/main/java/com/iss/Hellogit.java
@@ -4,8 +4,6 @@ public class Hellogit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("更新1");
-        System.out.println("创建分支");
-        System.out.println("主干添加");
 
     }
 }
